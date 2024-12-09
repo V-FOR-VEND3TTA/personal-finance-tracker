044acf4d2a97eef044c7cfc95f6a4d33ed01f1ff
George Hlongwane <georgehlongwane8@gmail.com>
2024-12-09 13:42:38 +0200

    Created models for our finance tracker

--- personal_finance_project/finance_tracker/models.py
+++ personal_finance_project/finance_tracker/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass
+
+class Transaction(models.Model):
+    TYPE_CHOICES = [('income', 'Income'), ('expense', 'Expense')]
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    category = models.CharField(max_length=50)
+    date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+
+class Budget(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
