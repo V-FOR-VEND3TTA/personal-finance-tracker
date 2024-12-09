2548ffdd7838802b37f7d1d122e9db87a2c8eb95
George Hlongwane <georgehlongwane8@gmail.com>
2024-12-09 13:53:23 +0200

    Created import statements for our pending views

--- personal_finance_project/finance_tracker/views.py
+++ personal_finance_project/finance_tracker/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .models import Transaction, Budget
+from .forms import TransactionForm, BudgetForm
