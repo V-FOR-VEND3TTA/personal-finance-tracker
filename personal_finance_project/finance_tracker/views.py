d5e4c182e4bd8b8f626c2b1fd8806eda8d3cd0ec
George Hlongwane <georgehlongwane8@gmail.com>
2024-12-09 13:57:15 +0200

    Created transaction list view

--- personal_finance_project/finance_tracker/views.py
+++ personal_finance_project/finance_tracker/views.py
@@ -5,3 +5,22 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Transaction, Budget
 from .forms import TransactionForm, BudgetForm
+from personal_finance_project.finance_tracker import models
+
+@method_decorator(login_required, name='dispatch')
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'transactions/transaction_list.html'
+    context_object_name = 'transactions'
+
+    def get_queryset(self):
+        # Only fetch transactions for the logged-in user
+        return Transaction.objects.filter(user=self.request.user).order_by('-date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add additional context for filters or summaries
+        context['income_total'] = self.get_queryset().filter(type='income').aggregate(models.Sum('amount'))['amount__sum'] or 0
+        context['expense_total'] = self.get_queryset().filter(type='expense').aggregate(models.Sum('amount'))['amount__sum'] or 0
+        return context
+
